19b5c3bccf7efcb22e9cbf88f5c2d82c47a6774e
kanhaiyavarnwal <varnwalkanhaiya@gmail.com>
2026-09-12 20:53:33 +0530

    test nodemailer

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -1,26 +1,26 @@
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
-const mailSender = async(email,title,body)=>{
-     try{
-        let transporter = nodemailer.createTransport({
-            service:"gmail",
-            auth:{
-                user : process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            }
-        });
-        let info = await transporter.sendMail({
-            from : "kanhaiya",
-            to: `${email}`,
-            subject:`${title}`,
-            html:`${body}`
-        })
+const mailSender = async (email, title, body) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+    let info = await transporter.sendMail({
+      from: "kanhaiya",
+      to: `${email}`,
+      subject: `${title}`,
+      html: `${body}`,
+    });
 
-        console.log("info from mailsender: ",info)
-        return info;
-     }catch(err){
-        console.log(err.message)
-     }
-}
+    // console.log("info from mailsender: ", info);
+    return info;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
-export {mailSender}
+export { mailSender };

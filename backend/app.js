c8c7ea4a8b7fc00e3e3430a20f6f9947c84f8e62
kanhaiyavarnwal <varnwalkanhaiya@gmail.com>
2026-09-25 14:16:47 +0530

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,28 +4,59 @@ import dotenv from "dotenv";
 import connectCloudinary from "./config/cloudinary.js";
 import connectDb from "./config/mongodb.js";
 import { ApiResponse } from "./utils/apiResponse.js";
+import express from "express"
 
-import { app2 } from "./app2.js";
-import userModel from "./models/userSchema.js";
+ import cookieParser from "cookie-parser";
+ import cors from "cors";
+ import productRouter from "./routes/productRoute.js";
+ import userRouter from "./routes/userRoute.js";
+ import orderRouter from "./routes/orderRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+
+ import userModel from "./models/userSchema.js";
 
 dotenv.config({
   path: "/.env",
 });
+const app = express()
 const port = process.env.PORT || 6000;
+  app.use(express.json());
+  app.use(cors());
+  app.use(cookieParser({}));
+  app.use(cookieParser({}));
+
+  app.use("/api/product", productRouter);
+  app.use("/api/user", userRouter);
+  app.use("/api/cart", cartRouter);
+  app.use("/api/order", orderRouter);
+
+
+  app.use((err, req, res, next) => {
+  console.log("ERROR:", err);
+
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Something went wrong",
+    error: err.error || [],
+  });
+});
+
 
 connectCloudinary();
 connectDb()
   .then(() => {
-    app2.get("/user", (req, res) => {
+    app.get("/user", (req, res) => {
       return res.json(
         new ApiResponse(203, {userModel }, "your server work"),
       );
     });
 
-    app2.listen(port, (req, res) => {
+    app.listen(port, (req, res) => {
       console.log(` your port no is runn ${port}`);
     });
   })
   .catch((err) => {
-    console.log("facing technical issue in server.js");
+    console.log("facing technical issue in server.js",err.message);
   });
+
+
